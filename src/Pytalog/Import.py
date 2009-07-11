fdcfc07c3c1367fdb4e5479aba32b408aa4a76b6
santiago iturriaga <santiago.iturriaga@gmail.com>
2009-07-10 21:48:56 -0300

    (no commit message)

---
+++ src/Pytalog/Import.py
@@ -0,0 +1,44 @@
+'''
+Created on Jul 10, 2009
+
+@author: santiago
+'''
+
+import sys, gtk
+
+class Import(object):
+    '''
+    Import window code-behind
+    '''
+
+
+    def __init__(self, principal):
+        self.__principal = principal
+        
+        self.__builder = gtk.Builder()
+        self.__builder.add_from_file("import.glade")
+        self.__builder.connect_signals(self)
+        
+        self.__assistant = self.__builder.get_object("assistant")        
+        #self.__assistant.set_forward_page_func(self.page_func)
+        
+    def show(self):
+        self.__assistant.show()
+        
+    def on_assistant_apply(self, widget, data=None):
+        print "on_assistant_apply"
+    
+    def on_assistant_cancel(self, widget, data=None):
+        print "on_assistant_cancel"
+        self.__assistant.destroy()
+    
+    def on_assistant_close(self, widget, data=None):
+        print "on_assistant_close"
+        self.__assistant.destroy()
+    
+    def on_assistant_prepare(self, widget, page, data=None):
+        print "on_assistant_prepare"
+        self.__assistant.set_page_complete(page, True)
+    
+    def page_func(self, current_page, user_data):
+        print "page_func"
